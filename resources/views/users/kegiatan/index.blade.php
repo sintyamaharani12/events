0d8e3fef529fa1db65637858e69ac603eb946d23
sintyamaharani12 <sintyamaharani34@gmail.com>
2020-11-06 10:10:48 +0700

    kegiatan

---
+++ resources/views/users/kegiatan/index.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb bg-white">
+                    <li class="breadcrumb-item active" aria-current="page">
+                        <li class="badge badge-info">
+                            Aktif
+                        </li>
+                    </li>
+                </ol>
+            </nav>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card-border-0">
+                        <div class="card-body">
+                            <div class="d-flex">
+                            </div>
+                        <img src="{{asset('banner/iya.png')}}" alt="" class="rounded-circle"
+                         width="80" 
+                         heigth="80" 
+                         srcset="">
+                        </div>
+                    <div class="card-body">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, deserunt nostrum doloribus repudiandae molestias, totam natus veritatis illo repellat aperiam animi rerum, est quaerat vitae ut quas voluptates quisquam ex.</p>
+                        <button type="submit" class="btn btn-outline-info btn-lg btn-block" disabled>Daftar</button>
+                       </div>
+                    </div>
+                </div>
+                        
+                <div class="row">
+                <div class="col-md-12">
+                    <div class="card-border-0">
+                        <div class="card-body">
+                            <div class="d-flex">
+                            </div>
+                        <img src="{{asset('banner/iya.png')}}" alt="" class="rounded-circle"
+                         width="80" 
+                         heigth="80" 
+                         srcset="">
+                        </div>
+                    <div class="card-body">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, deserunt nostrum doloribus repudiandae molestias, totam natus veritatis illo repellat aperiam animi rerum, est quaerat vitae ut quas voluptates quisquam ex.</p>
+                        <button type="submit" class="btn btn-outline-info btn-lg btn-block" disabled>Daftar</button>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card-border-0">
+                        <div class="card-body">
+                            <div class="d-flex">
+                            </div>
+                        <img src="{{asset('banner/iya.png')}}" alt="" class="rounded-circle"
+                         width="80" 
+                         heigth="80" 
+                         srcset="">
+                        </div>
+                    <div class="card-body">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, deserunt nostrum doloribus repudiandae molestias, totam natus veritatis illo repellat aperiam animi rerum, est quaerat vitae ut quas voluptates quisquam ex.</p>
+                        <button type="submit" class="btn btn-outline-info btn-lg btn-block" disabled>Daftar</button>
+                       </div>
+                    </div>
+                </div>
+                <div class="row">
+                <div class="col-md-12">
+                    <div class="card-border-0">
+                        <div class="card-body">
+                            <div class="d-flex">
+                            </div>
+                        <img src="{{asset('banner/iya.png')}}" alt="" class="rounded-circle"
+                         width="80" 
+                         heigth="80" 
+                         srcset="">
+                        </div>
+                    <div class="card-body">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, deserunt nostrum doloribus repudiandae molestias, totam natus veritatis illo repellat aperiam animi rerum, est quaerat vitae ut quas voluptates quisquam ex.</p>
+                        <button type="submit" class="btn btn-outline-info btn-lg btn-block" disabled>Daftar</button>
+                       </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
